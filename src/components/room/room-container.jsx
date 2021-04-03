a448b30600a24967779ffa90dee5efba6280b195
Anna Levchenko <levallevchenko@gmail.com>
2021-04-03 21:06:50 +0300

    Вернёт useParams на место

--- src/components/room/room-container.jsx
+++ src/components/room/room-container.jsx
@@ -1,20 +1,21 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {fetchNearbyOffers, fetchOffer, fetchReviews} from '../../store/api-actions';
+import {useParams} from 'react-router';
 import LoadingScreen from '../loading-screen/loading-screen';
 import Room from './room';
 import NotFound from '../not-found/not-found';
 
 const RoomContainer = () => {
   const {offer, reviews, nearbyOffers, offerNotFound} = useSelector((state) => state.OFFERS);
-  const {hotelId} = offer;
+  const {id} = useParams();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchOffer(hotelId));
-    dispatch(fetchReviews(hotelId));
-    dispatch(fetchNearbyOffers(hotelId));
-  }, [hotelId]);
+    dispatch(fetchOffer(id));
+    dispatch(fetchReviews(id));
+    dispatch(fetchNearbyOffers(id));
+  }, [id]);
 
   if (offerNotFound) {
     return <NotFound />;
