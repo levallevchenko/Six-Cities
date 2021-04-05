5618fa680b9f7d000206fd1618d9024fa28aba6c
Anna Levchenko <levallevchenko@gmail.com>
2021-04-05 12:35:21 +0300

    Поправит тест Favorites после изменений

--- src/components/favorites/favorites.test.js
+++ src/components/favorites/favorites.test.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
+import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 import * as redux from 'react-redux';
 import {NameSpace} from '../../store/root-reducer';
 import Favorites from './favorites';
 
-const mockStore = configureStore();
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 jest.spyOn(redux, `useSelector`);
 jest.spyOn(redux, `useDispatch`);
@@ -32,6 +34,7 @@ it(`Favorites should render correctly`, () => {
     },
   });
 
+  store.dispatch = () => Promise.resolve();
   const history = createMemoryHistory();
 
   render(
